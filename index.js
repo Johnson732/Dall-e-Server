ed8ac8ba9feec125a486c28df8d5410253498af5
Johnson <johnson266it@gmail.com>
2024-04-26 09:54:00 +0530

    dalle gpt integrated server side

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const app = express();
-
+require('dotenv').config();
 app.use(cors());
 app.use(express.json());
-console.log("backend1");
-let url = "https://api.openai.com/v1/images/generations";
-let token = "";
+//console.log("backend1");
+let imageUrl = "https://api.openai.com/v1/images/generations";
+let chatUrl ="https://api.openai.com/v1/chat/completions";
+let token = process.env.TOKEN;
 app.post("/api", async (req, res) => {
-  console.log("backend2");
+  //console.log("backend2");
   const prompt = req.body.prompt;
   try {
     const response = await axios.post(
-      url,
+      imageUrl,
       {
         prompt: prompt,
         model: "dall-e-3",
@@ -27,10 +28,40 @@ app.post("/api", async (req, res) => {
         },
       }
     );
-    console.log("backend3");
+    //console.log("backend3");
     res.json(response.data);
   } catch (error) {
     console.error("Error:", error.response.data);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/chat", async (req, res) => {
+  //console.log("backend2,");
+  const prompt = `can you list a 10 different prompts for this given prompt to generate an image "${req.body.prompt}"`;
+  try {
+    const response = await axios.post(
+      chatUrl,
+      {
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: prompt,
+          },
+        ],
+      },
+      {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    //console.log("backend3,");
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
